866166cc96a531e780a1cb367141a2b63f53fef5
SLGabi <gdslucas@inf.ufpel.edu.br>
2021-05-27 18:50:23 -0300

    em andamento

--- Lista 4/exercicio1.c
+++ Lista 4/exercicio1.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
+void listarVetor(int *vetor, int tam);
+int limparVetor(int *vetor);
 void insertionSort(int *vetor, int tam);
 void selectionSort(int *vetor, int tam);
 void quickSort(int *vetor, int esquerda, int direita);
-void mergeSort(int *vetor);
+void mergeSort(int *vetor, int inicio, int fim);
+void merge(int *vetor, int inicio, int meio, int fim);
 
 int main(){
-    int tam, random, op;
-    srand(time(NULL));
-
-    printf("Qual o tamanho do vetor que desejas?\n");
-    scanf("%d", &tam);
-    int *vetor = malloc(sizeof(int) * tam);
-
-    for(int i=0; i<tam; i++){
-        random = rand()%1000;
-        vetor[i] = random;
-    }
-
-    for(int k=0; k<tam; k++){
-        printf("%d\n", vetor[k]);
-    }
-
+    int *vetor, tam = 0, random, op;
     do{
+        if(tam == 0){
+            srand(time(NULL));
+
+            printf("Qual o tamanho do vetor que desejas?\n");
+            scanf("%d", &tam);
+            vetor = malloc(sizeof(int) * tam);
+
+            for(int i=0; i<tam; i++){
+                random = rand()%1000;
+                vetor[i] = random;
+            }
+        }
+
         printf("<1> Insertion Sort\n");
         printf("<2> Selection Sort\n");
         printf("<3> Quick Sort\n");
         printf("<4> Merge sort\n");
-        printf("<5> Sair\n");
+        printf("<5> Listar vetor\n");
+        printf("<6> Limpar vetor\n");
+        printf("<7> Sair\n");
         scanf("%d", &op);
         
         switch(op){
@@ -43,16 +47,22 @@ int main(){
 
             case 3:
             quickSort(vetor, 0, tam-1);
-            for(int k=0; k<tam; k++){
-                printf("%d\n", vetor[k]);
-            }
             break;
 
-            /*case 4:
-            mergeSort(vetor, tam);
+            case 4:
+            mergeSort(vetor, 0, tam-1);
             break;
-            */
+            
+
             case 5:
+            listarVetor(vetor, tam);
+            break;
+
+            case 6:
+            tam = limparVetor(vetor);
+            break;
+
+            case 7:
             free(vetor);
             exit(1);
             break;
@@ -61,7 +71,22 @@ int main(){
             printf("opcao invalida.\n");
             break;
         }
-    }while(op!=5);
+    }while(op!=7);
+}
+
+int limparVetor(int *vetor){
+    if(vetor != NULL){
+        free(vetor);
+        return 0;
+    }
+    else
+        return 0;
+}
+
+void listarVetor(int *vetor, int tam){
+    for(int k=0; k<tam; k++){
+        printf("%d\n", vetor[k]);
+    }
 }
 
 void insertionSort(int *vetor, int tam){
@@ -77,9 +102,6 @@ void insertionSort(int *vetor, int tam){
         vetor[j+1] = x;
     }
 
-    for(int k=0; k<tam; k++){
-        printf("%d\n", vetor[k]);
-    }
 }
 
 void selectionSort(int *vetor, int tam){
@@ -110,7 +132,7 @@ void quickSort(int *vetor, int esquerda, int direita){
     int meio,temp,i,j;
     i = esquerda;
     j = direita;
-    meio = vetor[(esquerda + direita)/2];
+    meio = vetor[(esquerda + rand()%direita)];
     do{
         while(vetor[i] < meio)
             i++;
@@ -128,4 +150,50 @@ void quickSort(int *vetor, int esquerda, int direita){
             quickSort(vetor,esquerda,j);
         if(i < direita)
             quickSort(vetor,i,direita);
+}
+
+void mergeSort(int *vetor, int inicio, int fim){
+    int meio;
+    if(inicio < fim){
+        meio = floor((inicio+fim)/2);
+        mergeSort(vetor, inicio, meio);
+        mergeSort(vetor, meio + 1, fim);
+        merge(vetor, inicio, meio, fim);
+    }
+}
+
+void merge(int *vetor, int inicio, int meio, int fim){
+    int *temp, n1, n2, tamanho, i, j, k;
+    int fim1 = 0, fim2 = 0;
+    tamanho = fim - inicio + 1;
+    n1 = inicio;
+    n2 = meio+1;
+    temp = malloc(sizeof(int)*tamanho);
+
+    if(temp!=NULL){
+        for(i=0; i<tamanho; i++){
+            if(!fim1 && !fim2){
+                if(vetor[n1] < vetor[n2]) //adicionando no vetor temporario ordenado
+                    temp = vetor[n1++];
+                else
+                    temp = vetor[n2++];
+
+                if(n1>meio)
+                    fim1 = 1;
+                if(n2>fim)
+                    fim2=1;
+            }
+            else{ //o que sobrou copia pro temp
+                if(!fim1)
+                    temp[i] = vetor[n1++];
+                else
+                    temp[i] = vetor[n2++];
+            }
+        }
+
+        for(j=0, k=inicio; j<tamanho; j++, k++){ //copia ordenado pro vetor de novo
+            vetor[k] = temp[j];
+        }
+    }
+    free(temp);
 }
